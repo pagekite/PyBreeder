b74b676364581f072aaecfd36146bb20c4a09d34
Bjarni R. Einarsson <bre@klaki.net>
2015-09-20 15:41:13 +0100

    Add README fun to test

--- test/__main__.py
+++ test/__main__.py
@@ -1,5 +1,10 @@
 from testing.fun import *
 from testing.work import *
 
+try:
+    print open('.SELF/README.md').read()
+except (IOError, OSError):
+    print "Rats, no README"
+
 print Fun()
 print Work()
